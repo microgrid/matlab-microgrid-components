78683c5e7fc37e2f709f6abb8ad06176b0a5e273
Jeemijn <jeemijnscheen@gmail.com>
2015-08-30 14:30:03 +0200

    saved shifted load profiles

--- src/generate_shifted_load_profile.m
+++ src/generate_shifted_load_profile.m
@@ -16,19 +16,20 @@ irr = importdata([path_to_dataBase, 'solar_data_Phuntsholing_baseline.mat']);   
 irr_scaled = irr .* 100;
 
 % shift the load profile
-Load_3_hrs_earlier = Load;                                     % vector to make a new Load profile that is shifted to the left by 3 hours
+Load_3_hrs_earlier = Load;                                  % vector to make a new Load profile that is shifted to the left by 3 hours
 Load_6_hrs_earlier = Load;
 
-first_3_hrs = Load(1:3);                                            % save first 3 hours of the year
-Load_3_hrs_earlier(1:3) = [];                                  % delete first 3 hours of the year from Load profile
-Load_3_hrs_earlier = [Load_3_hrs_earlier first_3_hrs];    % add the first 3 hours at the end of the year in order to have a full year again (with same total energy) 
+first_3_hrs = Load(1:3);                                    % save first 3 hours of the year
+Load_3_hrs_earlier(1:3) = [];                               % delete first 3 hours of the year from Load profile
+Load_3_hrs_earlier = [Load_3_hrs_earlier first_3_hrs];      % add the first 3 hours at the end of the year in order to have a full year again (with same total energy) 
 
-first_6_hrs = Load(1:6);                                            % repeat procedure for a shift of 6 hours to the left
+first_6_hrs = Load(1:6);                                    % repeat procedure for a shift of 6 hours to the left
 Load_6_hrs_earlier(1:6) = [];                                  
 Load_6_hrs_earlier = [Load_6_hrs_earlier first_6_hrs];    
 
 % save shifted load profiles
-
+save([path_to_dataBase,'LoadCurve_3_hrs_earlier.mat'],'Load_3_hrs_earlier')
+save([path_to_dataBase,'LoadCurve_6_hrs_earlier.mat'],'Load_6_hrs_earlier')
 
 % plot irradiation and average daily load profiles to check what happened
 irr_scaled_av = DailyAverage(irr_scaled);
@@ -36,20 +37,16 @@ Load_av = DailyAverage(Load);
 Load_av_3_hrs_earlier = DailyAverage(Load_3_hrs_earlier);
 Load_av_6_hrs_earlier = DailyAverage(Load_6_hrs_earlier);
 
-
 figure(10)
-plot(irr_scaled_av)
+plot(irr_scaled_av,'r')
 hold on
-plot(Load_av)
+plot(Load_av,'b')
 hold on
-plot(Load_av_3_hrs_earlier)
+plot(Load_av_3_hrs_earlier,'b--')
 hold on
-plot(Load_av_6_hrs_earlier)
+plot(Load_av_6_hrs_earlier,'b:')
 hold off
 xlabel('time (hour)')
 set(gca,'FontSize',12,'FontName','Times New Roman','fontWeight','bold')
 title('An average day')
-legend('irradiation x 100', 'Load', 'Load 3 hrs to left', 'Load 6 hrs to left')
-
-%todo: colors and save load profiles
-
+legend('irradiation x 100', 'Load', 'Load 3 hrs to left', 'Load 6 hrs to left','Location','northwest')
